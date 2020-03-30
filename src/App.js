76c03d8e0a8b28d5cf94776ac300cf974b394d10
hr-han <hr-han@users.noreply.github.com>
2020-03-30 21:11:48 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+
+
+function Food({fav}) {
+  console.log(fav);
+  
+return <h1>I like {fav}!</h1>
+}
+
 function App() {
   return (
     <div className="App">
-      Hello!!
+      <h1>Hello!!</h1>
+      <Food favorite="dduckbbockki"/>
+      <Food fav="ramen" />
+      <Food favorite="americano" />
+      <Food fav="dduck" />
     </div>
   );
 }
