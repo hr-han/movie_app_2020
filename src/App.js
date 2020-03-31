223b94f5ff7ad20f1172c4ed14aae0af9217f862
hr-han <hr-han@users.noreply.github.com>
2020-03-31 21:54:54 +0900

    propTypes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
+const foodILike = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+      rating:5
+      
+  },
+  {
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+      rating:5
+  },
+  {
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+      rating:2
+  },
+  {
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+      rating:"4"
+  },
+  {
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+  }
+];
 
+function Food({ name, img, rating }) {
+  return <div>
+    <h2>I like {name}!</h2>
+    <h4>{rating}/10</h4>
+    <img src={img} alt={name}/>
+  </div>
+}
 
-function Food({fav}) {
-  console.log(fav);
-  
-return <h1>I like {fav}!</h1>
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  rating: PropTypes.number
 }
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!</h1>
-      <Food favorite="dduckbbockki"/>
-      <Food fav="ramen" />
-      <Food favorite="americano" />
-      <Food fav="dduck" />
+      {foodILike.map((dish, idx) => (
+        <Food key={idx} name={dish.name} img={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
